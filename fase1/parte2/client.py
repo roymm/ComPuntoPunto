b0369a87f98d30a39479d02ea639d7233c55d4ca
sebas3552 <--global>
2019-03-23 21:59:23 -0600

    Ya hay conectividad entre cliente y servidor

--- fase1/parte2/client.py
+++ fase1/parte2/client.py
@@ -1,10 +1,31 @@
-from server import validate_port
-from server import read_port
+from socket import *
 
+# Function that reads in the port entered by the user, in command prompt.
+def read_port():
+	port = int(input('Enter the port number: '))
+	return port
+
+# Function that returns true if port_number is valid, false otherwise.
+# A valid port must be an integer in range [1024, 65535].
+def validate_port(port_number):
+	try:
+		int(port_number)
+	except Exception as error:
+		print(error)
+		return False
+
+	if port_number > 1023 and port_number < 65536:
+		return True
+	else:
+		return False
+		
+# Function that reads in the IP address entered by the user, in command prompt.
 def read_ip_address():
 	ip_address = str(input('Enter the server IP adress: '))
 	return ip_address
-	
+
+# Function that returns true if ip_address is a valid IPv4 address, false otherwise.
+# A valid IPv4 address must contain four number blocks separed by points, where each number is in range [0, 255]. Ex: 192.168.0.1
 def validate_ip_address(ip_address):
 	if not ip_address:
 		return False
@@ -25,10 +46,29 @@ def validate_ip_address(ip_address):
 			return False
 	
 	return True
-	
-#Code for testing the above functions
 
-if validate_ip_address(read_ip_address()) and validate_port(read_port()):
-	print('We can start communication!')
+# Function that provides an open socket, given the server ip address and the port number to communicate.	
+def connect_to_server(ip_address, port_number):
+	try:
+		client_socket = socket(AF_INET, SOCK_STREAM)
+		client_socket.connect((ip_address, port_number))
+	except Exception as error:
+		print(error)
+		return 0
+	return client_socket
+	
+	
+# Code for testing the above functions
+
+user_ip_address = read_ip_address()
+user_port = read_port()
+
+if validate_ip_address(user_ip_address) and validate_port(user_port):
+	print('Client can start communication!')
+	open_socket = connect_to_server(user_ip_address, user_port)
+	open_socket.send(str('Connection established with ' + str(user_ip_address) + ' in port ' + str(user_port)).encode())
+	received_message = open_socket.recv(1024)
+	print('Received from the server: ' + received_message.decode())
+	open_socket.close()
 else:
 	print('We can NOT start communication!')
--- fase1/parte2/server.py
+++ fase1/parte2/server.py
@@ -1,10 +1,44 @@
+from socket import *
+
+# Function that reads in the port entered by the user, in command prompt.
 def read_port():
 	port = int(input('Enter the port number: '))
 	return port
 
+# Function that returns true if port_number is valid, false otherwise.
+# A valid port must be an integer in range [1024, 65535].
 def validate_port(port_number):
+	try:
+		int(port_number)
+	except Exception as error:
+		print(error)
+		return False
+
 	if port_number > 1023 and port_number < 65536:
 		return True
 	else:
 		return False
+		
+# Function that returns a 3-tuple containing an open socket, the client's IP address and the port number.
+def connect_to_client(port_number):
+	try:
+		server_socket = socket(AF_INET, SOCK_STREAM)
+		server_socket.bind(('', port_number))
+		server_socket.listen(1)
+		# accept() returns a 3-tuple containing the socket given by the server, the client's IP address and the port number.
+		connection_socket, client_address = server_socket.accept()
+	except Exception as error:
+		print(error)
+		return 0
+	return (connection_socket, client_address)
 	
+# Code for testing purposes
+user_port = read_port()
+if validate_port(user_port):
+	print('Server can start communication!')
+	client_information = connect_to_client(user_port)
+	open_socket, client_address = client_information[0], client_information[1]
+	received_message = open_socket.recv(1024)
+	print('Received from the client: ' + received_message.decode())
+	open_socket.send(str('Connection established with ' + str(client_address)).encode())
+	open_socket.close()
