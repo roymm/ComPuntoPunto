c82a5eb388c65137f97af40f30d56f974b89ea3e
Christian Asch <christian.asch4@gmail.com>
2019-04-06 00:10:28 -0600

    moved files

--- fase2/parte1/error_handler.c
+++
@@ -1,9 +0,0 @@
-#include "error_handler.h"
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-void error_exit(int my_error, const char * message){
-		perror(message);
-		exit(my_error);
-}
--- fase2/parte1/error_handler.h
+++
@@ -1,5 +0,0 @@
-#ifndef ERROR_HANDLER_H
-#define ERROR_HANDLER_H
-
-void error_exit(int my_error, const char * message);
-#endif 
